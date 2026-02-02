dca13f9c997d380bf0a4a6edb4a21e00bffd9d83
emilynewton <emilynewton@u.boisestate.edu>
2026-02-02 11:32:15 -0700

    finished first ping sweep activity -> adding flags for -h and -p

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 # Ping sweep the Lab
 
-base="onyxnode"
+pingsweep() {
+	echo "Starting ping sweep..."
+	base="onyxnode"
+	for q in {1..200}
+		do
+			curr="$base$q"
+			ping -c 1 $curr >> ping.log
+	done
+	echo "Done! Check the ping.log :)"
+}
 
-for q in {1..200}
-do
-	curr="$base$q"
-	ping -c 1 $curr >> ping.log
+while getopts ":ph" flag; do 
+	case $flag in 
+		h) # handle -h flag (displays usage)
+			echo "Help: This script accepts -p to perform a ping sweep and -h for help."
+			;;
+		p) # handle -p flag (performs ping sweep)
+			pingsweep 
+			;;  
+	esac 
 done
 
